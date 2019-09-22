1cc501d4f28bd1672a57a3a47c2ae62835839b19
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-22 19:49:53 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,9 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!" if age <= 12
+    greeting = 
+    age <= 12? 
+    "Happy Birthday #{kids_name}! You are now #{age} years old!" if age <= 12
   end
 end
       
