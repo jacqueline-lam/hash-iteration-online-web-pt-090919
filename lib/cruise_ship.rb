8b378f5f51679fab26875967157347464fc229ae
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-22 19:36:19 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,13 @@
-passengers = {
-suite_a: "Amanda Presley", 
-suite_b: "Seymour Hoffman", 
-suite_c: "Alfred Tennyson", 
-suite_d: "Charlie Chaplin", 
-suite_e: "Crumpet the Elf"
-}
+# passengers = {
+# suite_a: "Amanda Presley", 
+# suite_b: "Seymour Hoffman", 
+# suite_c: "Alfred Tennyson", 
+# suite_d: "Charlie Chaplin", 
+# suite_e: "Crumpet the Elf"
+# }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |suite, name| #|key,value| passed together into block
+      if suite == :suite_a && name.start_with
 end
